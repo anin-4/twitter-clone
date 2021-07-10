e73673a49e7e1214fbe9d9ce72d000fac5b79999
Anindya Ray <anindyaray44@gmail.com>
2021-07-10 13:38:13 +0530

    minor changes

--- server/routes/login.js
+++ server/routes/login.js
@@ -10,28 +10,32 @@ router.get("/", (req, res) => {
 
 router.post("/", async (req, res) => {
   //will check where the username is present in the db and also whether the username and password match
-  const { userName, password } = req.body;
-  const isUser = await User.findOne({ userName: userName });
-  if (isUser) {
-    const dbpassword = isUser.password;
-    const isPassword = await bcrypt.compare(password, dbpassword);
-    if (isPassword) {
-      const Userdetails = await User.findOne({ userName: userName });
-      const token = jwt.sign(
-        {
-          id: Userdetails._id,
-          username: Userdetails.userName,
-          email: Userdetails.email,
-        },
-        process.env.ACCESS_TOKEN,
-        { expiresIn: "1h" }
-      );
-      res.json({ authorization: token });
+  try {
+    const { userName, password } = req.body;
+    const isUser = await User.findOne({ userName: userName });
+    if (isUser) {
+      const dbpassword = isUser.password;
+      const isPassword = await bcrypt.compare(password, dbpassword);
+      if (isPassword) {
+        const Userdetails = await User.findOne({ userName: userName });
+        const token = jwt.sign(
+          {
+            id: Userdetails._id,
+            username: Userdetails.userName,
+            email: Userdetails.email,
+          },
+          process.env.ACCESS_TOKEN,
+          { expiresIn: "1h" }
+        );
+        res.json({ authorization: token });
+      } else {
+        res.json({ Error: "error" });
+      }
     } else {
-      res.json({ password: "error" });
+      res.json({ Error: "error" });
     }
-  } else {
-    res.json({ signup: true });
+  } catch (e) {
+    console.log(e);
   }
 });
 module.exports = router;
